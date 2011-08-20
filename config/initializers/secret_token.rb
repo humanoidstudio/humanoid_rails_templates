e03a5189290e376b0714f07deb8ad7b02f05aca3
Cody Nguyen <cody@vinova.sg>
2011-08-21 00:28:16 +0700

    initialize app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HumanoidGpt::Application.config.secret_token = '6f94b6d39794ab3656df31f2a9a304a949f01f21b6712655d2ed68898ebf767750dfe78d9252395cc8d426749655ab038e41dd122158cbcfc5edcb6e02eb8aea'
