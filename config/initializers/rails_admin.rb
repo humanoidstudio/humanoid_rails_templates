e6619e42c982559b1ed89adb7d2e06cbdc9c77ea
Cody Nguyen <cody.haind@gmail.com>
2011-08-21 15:40:36 +0700

    moved ckeditor back to public

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -2,7 +2,7 @@ RailsAdmin.config do |config|
   config.excluded_models = ["Ckeditor::Asset","Ckeditor::AttachmentFile","Ckeditor::Picture"]
   config.models do
       edit do
-        field :email do
+        field :email, :text do
           ckeditor true
         end
       end
