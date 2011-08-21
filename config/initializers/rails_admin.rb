af8b35b3e558546011b5da5147bd6256a6f3e68f
Cody Nguyen <cody.haind@gmail.com>
2011-08-21 15:51:12 +0700

    config ckeditor & rails_admin

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -1,10 +1,4 @@
 RailsAdmin.config do |config|
   config.excluded_models = ["Ckeditor::Asset","Ckeditor::AttachmentFile","Ckeditor::Picture"]
-  config.models do
-      edit do
-        field :email, :text do
-          ckeditor true
-        end
-      end
-    end
+
 end
