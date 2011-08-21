71d5453989c3cecba92f6794d9add6251d085da2
Cody Nguyen <cody.haind@gmail.com>
2011-08-21 15:44:10 +0700

    added missing ckeditor files

--- public/javascripts/ckeditor/config.js
+++ public/javascripts/ckeditor/config.js
@@ -8,49 +8,4 @@ CKEDITOR.editorConfig = function( config )
 	// Define changes to default configuration here. For example:
 	// config.language = 'fr';
 	// config.uiColor = '#AADC6E';
-	
-  /* Filebrowser routes */
-  // The location of an external file browser, that should be launched when "Browse Server" button is pressed.
-  config.filebrowserBrowseUrl = "/ckeditor/attachment_files";
-
-  // The location of an external file browser, that should be launched when "Browse Server" button is pressed in the Flash dialog.
-  config.filebrowserFlashBrowseUrl = "/ckeditor/attachment_files";
-
-  // The location of a script that handles file uploads in the Flash dialog.
-  config.filebrowserFlashUploadUrl = "/ckeditor/attachment_files";
-  
-  // The location of an external file browser, that should be launched when "Browse Server" button is pressed in the Link tab of Image dialog.
-  config.filebrowserImageBrowseLinkUrl = "/ckeditor/pictures";
-
-  // The location of an external file browser, that should be launched when "Browse Server" button is pressed in the Image dialog.
-  config.filebrowserImageBrowseUrl = "/ckeditor/pictures";
-
-  // The location of a script that handles file uploads in the Image dialog.
-  config.filebrowserImageUploadUrl = "/ckeditor/pictures";
-  
-  // The location of a script that handles file uploads.
-  config.filebrowserUploadUrl = "/ckeditor/attachment_files";
-  
-  /* Extra plugins */
-  // works only with en, ru, uk locales
-  config.extraPlugins = "embed,attachment";
-  
-  /* Toolbars */
-  config.toolbar = 'Easy';
-  config.height = '400px';
-  config.width = '600px';
-  
-  config.toolbar_Easy =
-    [
-        ['Cut','Copy','Paste','PasteText','PasteFromWord',],
-        ['Maximize','-','About'],
-        ['Undo','Redo','-','Find','Replace','-','SelectAll','RemoveFormat'],
-        ['Styles','Format'],
-        ['Bold','Italic','Underline','Strike','-','Subscript','Superscript', 'TextColor'],
-        ['NumberedList','BulletedList','-','Outdent','Indent','Blockquote'],
-        ['JustifyLeft','JustifyCenter','JustifyRight','JustifyBlock'],
-        ['Link','Unlink','Anchor'],
-        ['Image','Embed','Flash','Attachment','Table','HorizontalRule','Smiley','SpecialChar','PageBreak'],
-        ['Source','-','Preview','Templates']
-    ];
 };
